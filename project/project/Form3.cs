96aef9a4963b495cec5dabef1b2f7ef01b605f8a
Nickname5713 <0720667@schoolsnet.act.edu.au>
2025-05-22 15:12:04 +1000

    Add files via upload

---
+++ project/project/Form3.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project
+{
+    public partial class Form3 : Form
+    {
+        public static string complete1 = "incomplete";
+        public static string complete2 = "incomplete";
+        public static string complete3 = "incomplete";
+        public static string complete4 = "incomplete";
+        public Form3()
+        {
+            InitializeComponent();
+            label4.Text = complete1;
+            label3.Text = complete2;
+            label2.Text = complete3;
+            label1.Text = complete4;
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form4 f4 = new Form4();
+            f4.Owner = this;
+            f4.StartPosition = FormStartPosition.Manual;
+            f4.Location = this.Location;
+            f4.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form5 f5 = new Form5();
+            f5.Owner = this;
+            f5.StartPosition = FormStartPosition.Manual;
+            f5.Location = this.Location;
+            f5.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Owner = this;
+            f2.StartPosition = FormStartPosition.Manual;
+            f2.Location = this.Location;
+            f2.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.Owner = this;
+            f1.StartPosition = FormStartPosition.Manual;
+            f1.Location = this.Location;
+            f1.Show();
+            this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+                Application.Exit();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Form6 f6 = new Form6();
+            f6.Owner = this;
+            f6.StartPosition = FormStartPosition.Manual;
+            f6.Location = this.Location;
+            f6.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ project/project/Form4.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project
+{
+
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+            label1.Text = "There are many things you can do with variables, such as having strings of variables. You can call the strings later and have \n them written in full or you can check strings for various words. One of the best things about strings is that you can easily \nconvert a user's input into a string and then check it. The next page will allow you to check if a string input of yours contains\n a particular word, much like you can do for other users.";
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form3.complete1 = "complete";
+            Form2 f2 = new Form2();
+            f2.Owner = this;
+            f2.StartPosition = FormStartPosition.Manual;
+            f2.Location = this.Location;
+            f2.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3.complete1 = "complete";
+            Form3 f3 = new Form3();
+            f3.Owner = this;
+            f3.StartPosition = FormStartPosition.Manual;
+            f3.Location = this.Location;
+            f3.Show();
+            this.Hide();
+        }
+    }
+}
